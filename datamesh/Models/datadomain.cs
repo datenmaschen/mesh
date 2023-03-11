527aa2b1c9d121ab19f9e325905088efb298116e
Philipp Frenzel <pf@com-x-cha.com>
2023-03-11 20:30:55 +0000

    init api

---
+++ datamesh/Models/datadomain.cs
@@ -0,0 +1,11 @@
+namespace Models
+{
+public class Datadomai
+
+{
+public int Id { get; set; }
+public string Name { get; set; }
+public string Imageurl { get; set; }
+public decimal Price { get; set; }
+}
+}
