1c12ff730a257c54afc9f8dc2513799b576f60f6
Philipp Frenzel <pf@com-x-cha.com>
2023-03-24 19:45:46 +0000

    first working efc context

--- datamesh-api/Models/DatameshContext.cs
+++ datamesh-api/Models/DatameshContext.cs
@@ -1,8 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Datamesh.Models;
-
-public class DatameshContext : DbContext
+namespace Datamesh.Models
 {
-    
+
+    public class DatameshContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //connection string for sqlserver localhost database
+            optionsBuilder.UseSqlServer("Server=localhost;Database=datamesh;User Id=sa;Password=P@ssw0rd;");
+        }
+
+        public DbSet<DataDomain> DataDomainSet { get; set; }
+    }
+
 }
