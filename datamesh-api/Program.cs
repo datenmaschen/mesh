ffd034c40a31a156a3a140632c1e0e0b8e36d317
Philipp Frenzel <pf@com-x-cha.com>
2023-03-26 18:20:14 +0000

    fix

--- datamesh-api/Program.cs
+++ datamesh-api/Program.cs
@@ -1,6 +1,7 @@
+using Datamesh.Models;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
-using Datamesh.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
