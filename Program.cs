e4029c90c8a5cfcb432b94a46e6d41dcdf98d457
Philipp Frenzel <pf@com-x-cha.com>
2023-04-02 19:26:47 +0000

    clean structure

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-//app.Urls.Add("http://localhost:5000");
+app.Urls.Add("http://localhost:5000");
 
 app.MapGet("/", () => "Hello World!");
 
@@ -22,6 +22,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 //app.UseHttpsRedirection();
 
