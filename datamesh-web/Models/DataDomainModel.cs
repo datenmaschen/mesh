4ecaa1aa983c097eaf1a139aa164c4a0ba804ccd
Philipp Frenzel <pf@com-x-cha.com>
2023-05-02 07:43:45 +0000

    sync efcore version

--- datamesh-web/Models/DataDomainModel.cs
+++ datamesh-web/Models/DataDomainModel.cs
@@ -13,10 +13,11 @@ namespace datamesh_web.Models
         [StringLength(20, ErrorMessage = "Key length can't be more than 20.")]
         [Display(Name = "Data Domain Key")]
         public string Key { get; set; }
-        [Required(ErrorMessage = "NameAbbrevationLong is Required.")]
+        [Required(ErrorMessage = "Name Abbrevation Long is Required.")]
         [Display(Name = "Abbrevation Long Name")]
         public string NameAbbrevationLong { get; set; }
-        [Required(ErrorMessage = "NameAbbreviationShort is Required.")]
+        [Required(ErrorMessage = "Name Abbreviation Short is Required.")]
+        [StringLength(4, ErrorMessage = "Abbreviation Short length can't be more than 4.")]
         [Display(Name = "Abbrevation Short Name")]
         public string NameAbbreviationShort { get; set; }
         [Required(ErrorMessage = "SubscriptionName is Required.")]
